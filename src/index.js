7be8d609da5f2cd8a0ea0bf22dddd2588cf2cff2
LayneHansen <layne.d.hansen@gmail.com>
2022-06-15 21:57:50 -0400

    removed hash router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom';
 import App from './App';
-// import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
